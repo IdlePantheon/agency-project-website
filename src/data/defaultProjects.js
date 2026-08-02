25a07ef62b4f2b4ddad739fadc4cb001616ef910
IdlePantheon <victormain981@gmail.com>
2026-08-02 21:38:10 +0300

    new project

--- src/data/defaultProjects.js
+++ src/data/defaultProjects.js
@@ -6,7 +6,7 @@ const defaultProjects = [
     category: "Advertising & Print",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS10QpQTjGosxSVgquoM2D6q1MveW61dxnmXjdFLsv5DQ&s=10",
     description:
-      "A city-wide billboard and built around relatable Kenyan culture,leaving the brand etched into people's minds, driving potential sales and 22% increase in walk in during that month.",
+      "A city wide billboard and built around relatable Kenyan culture,leaving the brand etched into people's minds, driving potential sales and 22% increase in walk in during that month.",
   },
   {
     id: "p2",
@@ -14,7 +14,7 @@ const defaultProjects = [
     title: "YEA Mobile App",
     category: "Digital Product Design",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLoSyYilGO0Q0MpdYUkojC01R54Amd0OxJwCQZoCAFgQ&s=10",
-    description: "End-to-end UX overhaul of the mobile banking app, targeting youths between 18 to 34. The slick design is made to drift away from the polished babby boomer prefernce leaning into a more expressive color and features.",
+    description: "UI design of the mobile banking app, targeting youths between 18 to 34. The slick design is made to drift away from the polished babby boomer prefernce leaning into a more expressive color and features.",
   },
   {
     id: "p3",
@@ -22,7 +22,7 @@ const defaultProjects = [
     title: "Fleet Branding Rollout",
     category: "Brand Identity",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGfpKMYezb013Fg-qVu3xoeGp_qsqkNexe9Uy7axizNw&s=10",
-    description: "Lively and driver-partner visual identity system rolled out across Kenyan markets, built for quick, low-cost local printing.",
+    description: "Lively and driver-partner visual identity system rolled out across Kenyan markets, built for quick, low cost local printing.",
   },
   {
     id: "p4",
@@ -31,6 +31,14 @@ const defaultProjects = [
     category: "Marketing Campaign",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBwx0uv441ekq1DJFqVJDTvTHz3VHLtshjIjkwKoq2_Q&s=10",
     description: "A social-first campaign spotlighting real Glovo riders, generating over 3 million organic impressions across Nairobi and Mombasa.",
+  },
+  {
+    id: "p5",
+    client: "Safaricom",
+    title: "MyOneApp Rebrand",
+    category: "Digital Product Design",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH4vFwppbkeJjUWTM11pPVVE9Vivg7wtvUCSWQhBo-uA&s=10",
+    description: "UI design of the mobile money app, aiming to integrate all M-pesa and Safarciom sercvices in one application.",  
   },
 ];
 
