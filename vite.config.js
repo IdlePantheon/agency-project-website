e1b256309a32c5595ac2aecc6671948c1a39cfa4
IdlePantheon <victormain981@gmail.com>
2026-08-02 22:36:27 +0300

    typo fix

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/agency-projecy-website/', 
+  base: '/agency-project-website/', 
   test: {
     environment: 'jsdom',
     globals: true,
